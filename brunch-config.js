951f461f478b79f8ce974efb25098802ee21a5d5
Kieran <reynoldsktsr@gmail.com>
2017-02-15 20:50:51 -0800

    Initial Commit

---
+++ brunch-config.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+	paths: {
+		public: 'dist',
+		watched: ['app', 'lib']
+	},
+	files: {
+		javascripts: {
+			joinTo: {
+				'js/vendors.js': /^lib/,
+				'js/app.js': /^app/
+			}
+		},
+		stylesheets: {
+			joinTo: {
+				'css/vendors.css': /^lib/,
+				'css/app.css': /^app/
+			}
+		}
+	},
+	plugins: {
+		jshint: {
+			pattern: /^app[\\/].*\.js$/,
+			options: {
+				bitwise: true,
+				curly: true
+			},
+			globals: {
+				jQuery: true
+			},
+			warnOnly: true
+		},
+		postcss: {
+			processors: [require('autoprefixer')(['last 8 versions'])]
+		}
+	}
+}
